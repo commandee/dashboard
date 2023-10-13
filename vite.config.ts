fbf9be7203e0712be7e2060e146b16ef53eef0fd
AmeMeida <ame.pistache@gmail.com>
2023-10-13 18:02:59 -0300

    postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  css: {
+    postcss: {
+      plugins: [cssnano, autoprefixer]
+    }
+  }
 })
