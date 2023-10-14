953f65f1c3bc5e1eadd637c5c64f2aba619da4be
AmeMeida <ame.pistache@gmail.com>
2023-10-14 17:26:57 -0300

    tirando o flashbang

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import '@picocss/pico/css/pico.min.css'
 import "./global.css"
 import App from './App.svelte'
 
+const mountOn = document.getElementById('app')!;
+
 const app = new App({
-  target: document.getElementById('app')!,
+  target: mountOn
 })
+
+mountOn.classList.add("loaded");
 
 export default app
